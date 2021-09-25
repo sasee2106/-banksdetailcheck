15eb82419ada7f9632ce32384d0dd68cbb736b54
sasee2106 <saseedharun.kt@gmail.com>
2021-09-25 22:23:53 +0530

    debit details resolved

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -1,6 +1,12 @@
 package org.bank;
 
 public class AxisBank {
+	
+	
+	public void debitCard() {
+		System.out.println("Debit card 2%");
+		
+	}
 public void deposit () {
 	System.out.println("deposit 10%");
 }
