c46d665679675c51c6e8b48d8b4973756af2057e
sasee2106 <saseedharun.kt@gmail.com>
2021-09-25 22:18:53 +0530

    mistakes resolved

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -1,8 +1,15 @@
 package org.bank;
 
 public class AxisBank {
+	
+	public void creditcard() {
+		System.out.println("Credit Card Interest 8%");
+		
+
+	}
 public void deposit () {
 	System.out.println("deposit 10%");
+	
 }
 
 }
